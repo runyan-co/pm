1fa35bcef3a5fbb8797dd3734108ba3b4ec33b72
Alex Runyan <alex@runyan.co>
2024-01-05 13:12:18 -0500

    Version 2.1.0

--- includes/bootstrap.php
+++ includes/bootstrap.php
@@ -19,7 +19,7 @@ Container::setInstance($container = new Container);
 Container::getInstance()
     ->when(\ProcessMaker\Cli\Application::class)
     ->needs('$version')
-    ->give('2.0.1');
+    ->give('2.1.0');
 
 $app = \ProcessMaker\Cli\Facades\Application::getInstance();
 
